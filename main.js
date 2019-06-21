79dfda9a4daae0fa55e44902ea4f2ce37396609f
Francois Crispo-Sauve <franksauve@outlook.com>
2019-06-21 18:44:02 -0400

    version 1

---
+++ main.js
@@ -0,0 +1,29 @@
+const thumbnailImg = document.querySelector('#thumbnail');
+const description = document.querySelector('#description');
+const episodeH1 = document.querySelector('#episode')
+
+function spin() {
+    const roulette = document.querySelector('#roulette');
+    roulette.classList.add('roulette-spin');
+    setTimeout(function () {
+        roulette.classList.remove('roulette-spin');
+        getRandomEpisode();
+    }, 2000);
+}
+
+function getRandomEpisode() {
+
+
+    $.getJSON('data/episodes.json', function (friends) {
+        var season = Math.floor(Math.random() * 10);
+        var episode = Math.floor(Math.random() * friends.seasons[season].episodes.length);
+        console.log("Season var " + season);
+        console.log("Episode var " + episode);
+        console.log(friends.seasons[season].episodes[episode]);
+        thumbnailImg.src = friends.seasons[season].episodes[episode].image;
+        episodeH1.innerHTML = "<b>Season " + friends.seasons[season].number +
+                                " Episode " + friends.seasons[season].episodes[episode].number + ": </b>" +
+                                 friends.seasons[season].episodes[episode].title;
+        description.innerHTML = friends.seasons[season].episodes[episode].description;
+    });
+}

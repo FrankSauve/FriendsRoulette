75062b20705b568b0a8c2f50db6bedd00435d541
Francois Crispo-Sauve <franksauve@outlook.com>
2020-12-16 17:16:49 -0500

    Removed console logs

--- main.js
+++ main.js
@@ -15,9 +15,6 @@ function getRandomEpisode() {
     $.getJSON('data/episodes.json', function (friends) {
         var season = Math.floor(Math.random() * 10);
         var episode = Math.floor(Math.random() * friends.seasons[season].episodes.length);
-        console.log("Season var " + season);
-        console.log("Episode var " + episode);
-        console.log(friends.seasons[season].episodes[episode]);
         thumbnailImg.src = friends.seasons[season].episodes[episode].image;
         episodeH1.innerHTML = "<b>Season " + friends.seasons[season].number +
                                 " Episode " + friends.seasons[season].episodes[episode].number + ": </b>" +
